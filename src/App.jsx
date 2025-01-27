a0b81505df7d55aaae9a8bfab9e27e6d0e2d4310
shureiki <lxnnylqs44@gmail.com>
2025-01-27 02:19:50 +0100

    Add absolute path system

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,7 @@ import {
 
 import {
 	Colors,
-	DebugInstructions,
 	Header,
-	LearnMoreLinks,
-	ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 function Section({ children, title }) {
@@ -60,25 +57,7 @@ function App() {
 				contentInsetAdjustmentBehavior="automatic"
 				style={backgroundStyle}>
 				<Header />
-				<View
-					style={{
-						backgroundColor: isDarkMode ? Colors.black : Colors.white,
-					}}>
-					<Section title="Step One">
-						Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-						Du beurres.
-					</Section>
-					<Section title="See Your Changes">
-						<ReloadInstructions />
-					</Section>
-					<Section title="Debug">
-						<DebugInstructions />
-					</Section>
-					<Section title="Learn More">
-						Read the docs to discover what to do next:
-					</Section>
-					<LearnMoreLinks />
-				</View>
+				<TestComponent />
 			</ScrollView>
 		</SafeAreaView>
 	);
